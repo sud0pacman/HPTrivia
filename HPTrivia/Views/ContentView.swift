d282b16b3864cb1cb4216ff9e958e2be783a0f5a
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-05-22 02:29:03 +0500

    - Recent score - Oppacity transition

--- HPTrivia/Views/ContentView.swift
+++ HPTrivia/Views/ContentView.swift
@@ -53,6 +53,26 @@ struct ContentView: View {
                     
                     Spacer()
                     
+                    VStack {
+                        if animationViewsIn {
+                            VStack {
+                                Text("Recent Scores")
+                                    .font(.title2)
+                                
+                                Text("33")
+                                Text("27")
+                                Text("15")
+                            }
+                            .font(.title)
+                            .foregroundStyle(.white)
+                            .padding(.horizontal)
+                            .background(.black.opacity(0.7))
+                            .clipShape(.rect(cornerRadius: 15))
+                            .transition(.opacity)
+                        }
+                    }
+                    .animation(.linear(duration: 1).delay(4), value: animationViewsIn)
+                    
                     Spacer()
                     
                     HStack {
