13afb2f6f0e7f0ac5a3b6d3f758fef211816a4cb
Muhammad <96905615+sud0pacman@users.noreply.github.com>
2026-05-21 16:50:24 +0500

    Initial Commit

---
+++ HPTrivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HPTrivia
+//
+//  Created by Muhammad on 21/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Muhammad on 21/05/26.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
