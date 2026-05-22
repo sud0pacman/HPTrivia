f7d2d489ee6969f3f3b2ec385f0f8ed378fa23e0
Muhammad <96905615+sud0pacman@users.noreply.github.com>
2026-05-22 16:50:59 +0500

    Add GamePlay view to display initial game interface

---
+++ HPTrivia/Views/GamePlay.swift
@@ -0,0 +1,18 @@
+//
+//  GamePlay.swift
+//  HPTrivia
+//
+//  Created by Muhammad on 22/05/26.
+//
+
+import SwiftUI
+
+struct GamePlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GamePlay()
+}
