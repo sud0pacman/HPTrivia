383b9f1a27e0898f16d8bb2ee70abc6cb90e5f5e
Muhammad <96905615+sud0pacman@users.noreply.github.com>
2026-05-22 20:37:21 +0500

    - Sound effect - DispatchQueue

--- HPTrivia/Views/GamePlay.swift
+++ HPTrivia/Views/GamePlay.swift
@@ -6,8 +6,15 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct GamePlay: View {
+    @Environment(Game.self) var game
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var musicPlayer: AVAudioPlayer!
+    @State private var sfxPlayer: AVAudioPlayer!
+    
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -35,9 +42,50 @@ struct GamePlay: View {
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .onAppear {
+            game.startGame()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                playMusic()
+            }
+        }
+    }
+    
+    private func playMusic() {
+        let songs = ["let-the-mystery-unfold", "spellcraft", "hiding-place-in-the-forest", "deep-in-the-dell" ]
+        
+        let song = songs.randomElement()!
+        
+        let sound = Bundle.main.path(forResource: song, ofType: "mp3")
+        musicPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        musicPlayer.numberOfLoops = -1
+        musicPlayer.volume = 0.1
+        musicPlayer.play()
+    }
+    
+    private func playFlipSound() {
+        let sound = Bundle.main.path(forResource: "page-flip", ofType: "mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        sfxPlayer.numberOfLoops = -1
+        sfxPlayer.play()
+    }
+    
+    private func playwrongSound() {
+        let sound = Bundle.main.path(forResource: "negative-beeps", ofType: "mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        sfxPlayer.numberOfLoops = -1
+        sfxPlayer.play()
+    }
+    
+    private func playCorrectSound() {
+        let sound = Bundle.main.path(forResource: "magic-wand", ofType: "mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        sfxPlayer.numberOfLoops = -1
+        sfxPlayer.play()
     }
 }
 
 #Preview {
     GamePlay()
+        .environment(Game())
 }
