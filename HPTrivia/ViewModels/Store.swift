cb64ac865ca8fa96e0a8c13969b27afbe6dbd402
Muhammad <96905615+sud0pacman@users.noreply.github.com>
2026-06-03 11:10:10 +0500

    - Purchase - @unknown - Break

--- HPTrivia/ViewModels/Store.swift
+++ HPTrivia/ViewModels/Store.swift
@@ -28,6 +28,36 @@ class Store {
     }
     
     // Purchase a product
+    func purchase(_ product: Product) async {
+        do {
+            let result = try await product.purchase()
+            
+            switch result {
+            // Purchase successful, but now we need to verify receipt and transaction
+            case .success(let verificationResult):
+                switch verificationResult {
+                case .unverified(let signedType, let verificationError):
+                    print("❌ Error on \(signedType) : \(verificationError)")
+                case .verified(let signedType):
+                    purchased.insert(signedType.productID)
+                    
+                    await signedType.finish()
+                }
+                
+            // User cancalled or parent disapproved child's purchase request
+            case .userCancelled:
+                break
+                
+            // Waiting for some sort of approval
+            case .pending:
+                break
+            @unknown default:
+                break
+            }
+        } catch {
+            print("❌ Unable to purchase: \(error)")
+        }
+    }
     
     // Check for purchased products
     
