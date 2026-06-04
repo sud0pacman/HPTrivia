3bf4b3743877b904c513ae47a0ca1cda6b9ecab2
Muhammad <96905615+sud0pacman@users.noreply.github.com>
2026-06-04 12:46:14 +0500

    Add RecentScoresView to display recent player scores

---
+++ HPTrivia/Views/RecentScoresView.swift
@@ -0,0 +1,18 @@
+//
+//  RecentScoresView.swift
+//  HPTrivia
+//
+//  Created by Muhammad on 04/06/26.
+//
+
+import SwiftUI
+
+struct RecentScoresView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentScoresView()
+}
